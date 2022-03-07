21a62475b4f6ad7567546e7415c22def56906a22
Jesse Walling <jessephilipwalling@gmail.com>
2022-03-06 17:37:22 -0800

    added password confirmation and feedback for SignUp

--- frontend/src/SignUp.jsx
+++ frontend/src/SignUp.jsx
@@ -12,6 +12,7 @@ TODO: Global backend URL (this will be api.example.com in the future)
 function SignUp() {
     const [user, setUser] = useState("")
     const [pass, setPass] = useState("")
+    const [confirm, setConfirm] = useState("")
     const [ishuman, setIshuman] = useState(null)
     const captchaRef = useRef(null)
 
@@ -22,22 +23,26 @@ function SignUp() {
     const handleSubmit = (e) => {
         e.preventDefault()
         if (ishuman) {
-            axios({
-                method: 'post',
-                url: '/auth_register',
-                data: {
-                    username: user,
-                    password: pass
-                },
-                headers: {
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
-                }
-            }).then(function (response) {
-                console.log(response);
-            }).catch(function (error) {
-                console.log(error)
-            });
+            if (pass == confirm) {
+                axios({
+                    method: 'post',
+                    url: '/auth_register',
+                    data: {
+                        username: user,
+                        password: pass
+                    },
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+                    }
+                }).then(function (response) {
+                    console.log(response);
+                }).catch(function (error) {
+                    console.log(error)
+                });
+            } else {
+                alert("Your passwords do not match")
+            }
         } else {
             alert("Please complete the reCAPTCHA")
         }
@@ -50,6 +55,13 @@ function SignUp() {
                 <input value={user} onChange={(e) => setUser(e.target.value)} type="text" id="username" name="username"></input>
                 <label for="password">Password:</label>
                 <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" id="password" name="password"></input>
+                <label for="confirm_pass">Confirm Password:</label>
+                <input value={confirm} onChange={(e) => setConfirm(e.target.value)} type="password" id="confirm" name="confirm"></input>
+                {pass == confirm ? (
+                    <div>✅</div>
+                ) : (
+                    <div>❌</div>
+                )}
                 <HCaptcha 
                     sitekey="04c36887-2836-4c0d-8467-9c13577fd631"
                     onLoad={onLoad}
